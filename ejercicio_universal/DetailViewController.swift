fa4ba6bab54e2d880db37b3745ce00a50ed615e4
Esther Agulló <esther.agullo03@goumh.umh.es>
2020-11-14 18:10:07 +0100

    Initial Commit

---
+++ ejercicio_universal/DetailViewController.swift
@@ -0,0 +1,40 @@
+//
+//  DetailViewController.swift
+//  ejercicio_universal
+//
+//  Created by Esther Agulló Fidel on 14/11/2020.
+//  Copyright © 2020 Esther Agulló Fidel. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet weak var detailDescriptionLabel: UILabel!
+
+
+    func configureView() {
+        // Update the user interface for the detail item.
+        if let detail = detailItem {
+            if let label = detailDescriptionLabel {
+                label.text = detail.description
+            }
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        configureView()
+    }
+
+    var detailItem: NSDate? {
+        didSet {
+            // Update the view.
+            configureView()
+        }
+    }
+
+
+}
+
